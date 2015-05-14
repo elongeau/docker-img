f117128d6b18df02ac4a28bc56b4e82057f180b2
elongeau <emmanuel.longeau@gmail.com>
2015-05-14 22:44:06 +0200

    Suppression du répertoire boot2docker
    inutile comme je n'utilise plus la VM boot2docker
    et il n'a pas sa place dans ce repo !

--- boot2docker/bootlocal.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-PROJECTS='/home/docker/projects'
-VIM='/home/docker/vimHost'
-mkdir $PROJECTS
-mkdir $VIM
-sudo mount -t vboxsf -o uid=1000,gid=50 projects $PROJECTS
-sudo mount -t vboxsf -o uid=1000,gid=50 vim $VIM 
-cd $PROJECTS
-chmod +x $PROJECTS/run-scala.sh
